ac8d999706e127ff4b0bf1bdcecb84ab523e6c3b
Jun <geffrey90@hotmail.com>
2019-03-29 06:50:00 +0800

    added html file

--- src/main.go
+++ src/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type NewsAggPag struct { //
+	Title string
+	News  string
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/agg/", newsHandler)
 	log.Fatal(http.ListenAndServe(":12345", mux))
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello world</h1>")
+}
+
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	n := NewsAggPag{Title: "good title", News: "greatss news"}
+	t, _ := template.ParseFiles("template.html")
+	t.Execute(w, n)
 }
